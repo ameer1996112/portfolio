d39b715694de12d46d9ccbe759508492f2be875d
Ameer <ameer_1996112@hotmail.com>
2021-08-20 20:09:01 +0300

    updated seendEmail

--- sendEmail.php
+++ sendEmail.php
@@ -1,10 +1,13 @@
-// Pear Mail Library
+
+
 <?php
 if (isset($_POST["email"]) && isset($_POST["code"])){
 	$to=$_POST["email"];
 	$subject="Verify Code:";
-	$message="Your Verify Code:" . $_POST["code"];
+	$message="Your Verify Code:"$_POST["code"];
 	mail($to,$subject,$message);
 	echo "Send successfully";
 }
-?>
+mail($to, $subject, $message, $headers);
+
+die;
